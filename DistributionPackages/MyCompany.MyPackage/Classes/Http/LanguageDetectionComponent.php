c0aa62279b2bf9fa2e3016e10917f6a961ed14a1
Leo Preissegger <leo@webundco.com>
2020-04-15 16:17:00 +0200

    multi language

---
+++ DistributionPackages/MyCompany.MyPackage/Classes/Http/LanguageDetectionComponent.php
@@ -0,0 +1,29 @@
+<?php
+namespace MyCompany\MyPackage\Http;
+
+/*
+ * This file is part of the MyCompany.MyPackage package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\Component\ComponentContext;
+use Neos\Flow\Http\Component\ComponentInterface;
+use Neos\Flow\I18n\Service as I18nService;
+use Neos\Flow\I18n\Locale;
+
+class LanguageDetectionComponent implements ComponentInterface
+{
+
+    /**
+     * @Flow\Inject
+     * @var I18nService
+     */
+    protected $localizationService;
+
+    public function handle(ComponentContext $componentContext)
+    {
+        $locale = new Locale('en');
+        $this->localizationService->getConfiguration()->setCurrentLocale($locale);
+    }
+
+}
